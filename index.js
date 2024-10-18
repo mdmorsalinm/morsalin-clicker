e2281b09c1ebb41e54f7210c54ed27a5592ab18d
mdmorsalinm <99299011+mdmorsalinm@users.noreply.github.com>
2024-10-18 10:33:45 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 // Counter App
 var count = 0;
+var entries = "";
 
 let counter = document.getElementById("count-el")
 counter.innerHTML = count;
 
 function increment() {
-    count++;
+    var num = Math.floor(Math.random() * 21);
+    count += num;
     console.log(count);
+    counter.innerHTML = count;
+}
+
+function save() {
+    entries += count + " - ";
+    document.getElementById("entries").innerHTML = entries;
+}
+
+function leave() {
+    count--;
     counter.innerHTML = count;
 }
